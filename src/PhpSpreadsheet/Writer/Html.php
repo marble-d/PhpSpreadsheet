3705a207de8e47e9d6cebd4b58e70eecdb4b5eba
Frederic Delaunay <frederic.delaunay@manymakers.net>
2018-10-19 01:08:50 +0200

    FIX: remove superfluous page-break-before

--- src/PhpSpreadsheet/Writer/Html.php
+++ src/PhpSpreadsheet/Writer/Html.php
@@ -439,8 +439,9 @@ class Html extends BaseWriter
         $html = '';
 
         // Loop all sheets
-        $sheetId = 0;
-        foreach ($sheets as $sheet) {
+        end($sheets);
+        $sheets_lastID = key($sheets);
+        foreach ($sheets as $sheetId => $sheet) {
             // Write table header
             $html .= $this->generateTableHeader($sheet);
 
@@ -517,13 +518,10 @@ class Html extends BaseWriter
 
             // Writing PDF?
             if ($this->isPdf) {
-                if ($this->sheetIndex === null && $sheetId + 1 < $this->spreadsheet->getSheetCount()) {
+                if ($this->sheetIndex === null && $sheetId != $sheets_lastID) {
                     $html .= '<div style="page-break-before:always" />';
                 }
             }
-
-            // Next sheet
-            ++$sheetId;
         }
 
         return $html;
