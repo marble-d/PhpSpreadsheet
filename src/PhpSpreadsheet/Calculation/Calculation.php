a3a7d77ffe86096e67ea0bd96f263d1b51bf8571
Frederic Delaunay <frederic.delaunay@manymakers.net>
2020-03-27 12:02:18 +0100

    FIX: RE for named ranges

--- src/PhpSpreadsheet/Calculation/Calculation.php
+++ src/PhpSpreadsheet/Calculation/Calculation.php
@@ -27,7 +27,7 @@ class Calculation
     //    Cell reference (cell or range of cells, with or without a sheet reference)
     const CALCULATION_REGEXP_CELLREF = '((([^\s,!&%^\/\*\+<>=-]*)|(\'[^\']*\')|(\"[^\"]*\"))!)?\$?\b([a-z]{1,3})\$?(\d{1,7})(?![\w.])';
     //    Named Range of cells
-    const CALCULATION_REGEXP_NAMEDRANGE = '((([^\s,!&%^\/\*\+<>=-]*)|(\'[^\']*\')|(\"[^\"]*\"))!)?(([_A-Z]|\p{L}\p{M})[(_A-Z|\p{L}\p{M})0-9\.]*)';
+    const CALCULATION_REGEXP_NAMEDRANGE = '((([^\s,!&%^\/\*\+<>=-]*)|(\'[^\']*\')|(\"[^\"]*\"))!)?([_A-Z\p{L}\p{M}][_A-Z\p{L}\p{M}0-9\.]*)';
     //    Error
     const CALCULATION_REGEXP_ERROR = '\#[A-Z][A-Z0_\/]*[!\?]?';
 
